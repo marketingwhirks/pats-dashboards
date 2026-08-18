09f487cd75d97e260cee9e025f139a11f58c749c
marketingwhirks <marketingwhirks@gmail.com>
2026-08-18 17:06:21 +0000

    Connections: rebuild from registry - 309 clients, 282 flowing, 0 dead tokens, 4 unconnected, 4 open exceptions

--- needs_auth_data.js
+++ needs_auth_data.js
@@ -1,1 +1,1 @@
-var NEEDS_AUTH_DATA = {"generated":"2026-08-17T18:02:09.867162+00:00","token_run_date":"2026-08-17","live_count":284,"total":311,"reconnect":[],"authorize":[]};
+var NEEDS_AUTH_DATA = {"generated":"2026-08-18T17:05:46.795927+00:00","token_run_date":"2026-08-18","live_count":282,"total":309,"reconnect":[],"authorize":[{"name":"WEAKLEY & ASSOCIATES INC","sn":"WEA1775","qbo_file":"WEAKLEY & ASSOCIATES INC","realm_id":"","added_by":"auto (sync_pending_auth)"}]};
